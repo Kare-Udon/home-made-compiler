193a791b8102515e6a09b4fd0e1425ea9a3912d9
fmfjjjjkk <853723049@qq.com>
2023-04-10 18:12:21 +0800

    UPDATE lots of function to write

--- Function.h
+++ Function.h
@@ -4,6 +4,98 @@ using namespace std;
 
 
 bool IsNum(char s) {
+	if (isdigit(s) != 0)
+	{
+		return true;
+	}
+}
+bool IsSingleSymbol(char s){
+	if ((isdigit(s) != 0) || (isalpha(s) != 0))
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+bool IsDualSymbol(char first,char next) {
 
 }
-bool IsSingleSymbol(char s){}
+
+void SelectWord(string word,int num) {
+	if (IsNumber(word)) return;  //判断这个string是不是数字
+	if (Issingle_symbol(word)) return; //判断这个string是不是单符号
+	if (Isdual_symbol(word)) return;//判断这个string是不是多符号
+	if (Iskeywords(word)) return; //判断这个string是不是关键词
+	if (IsCorrectWord(word)) return; //判断这个string是不是正确的命名
+
+	//都不是就错的
+
+	cout << "ERROR  " << num << endl;
+}
+
+//bool值是正确，需要cout，再return true，否则return false
+bool IsNumber(string word) {
+	if (true) {
+		cout << endl;//记得输出 格式 <1,digit> 或 <1.2,float>
+		return true;
+	}
+	else return false;
+	
+}
+
+
+bool Issingle_symbol(string word) {
+
+	if (true) {
+		cout << endl;//记得输出 
+		return true;
+	}
+	else return false;
+}
+
+
+bool Isdual_symbol(string word) {
+
+	if (true) {
+		cout << endl;//记得输出 
+		return true;
+	}
+	else return false;
+}
+
+
+bool Iskeywords(string word) {
+
+	if (true) {
+		if (Istype_identifier(word)) return true;  //判断是不是类型 如 int char 之类的
+		else {
+			cout << endl;//记得输出
+		return true;
+		}
+		
+	}
+	else return false;
+
+}
+
+
+bool IsCorrectWord(string word) {
+
+	if (true) {
+		cout << endl;//记得输出 
+		return true;
+	}
+	else return false;
+}
+
+bool Istype_identifier(string word) {
+
+	if (true) {
+		cout << endl;//记得输出 
+		return true;
+	}
+	else return false;
+}
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<fstream>
 #include "Function.h"
 #include <list>
+#include "wordlist.h"
 using namespace std;
 
 
@@ -31,7 +32,7 @@ int main() {
             if (IsNum(line[j][i])) { //判断是不是数字 比如 12 1 1234
                 string s;
                 int same_i = i;
-                while (IsNum(line[j][same_i]))
+                while (IsNum(line[j][same_i])|| line[j][same_i]=='.')
                 {
                     s.push_back(line[j][same_i]);
                     same_i++;
@@ -46,18 +47,20 @@ int main() {
             else if (IsSingleSymbol(line[j][i])) {//判断是不是符号
                 string s;
                 s.push_back(line[j][i]);
-                if (IsSingleSymbol(line[j][i + 1]))//判断是不是二元运算符如 ++ --
+                i++;
+                if (IsDualSymbol(line[j][i-1],line[j][i]))//判断是不是二元运算符如 ++ -- ,两个参数，一个是第一位参数，一个是第二位参数
                 {
-                    s.push_back(line[j][i+1]);
+                    s.push_back(line[j][i]);
+                    i++;
                 }
                 line2.push_back(s);
-                i += 2;
+                
                 continue;
             }
             else { //剩余就是字符串 一直读，读到换行
                 string s;
                 int same_i = i;
-                while (same_i<length|| IsSingleSymbol(line[j][same_i]))//判断下一个是不是符号
+                while (same_i<length &&  !IsSingleSymbol(line[j][same_i]))//判断下一个是不是符号
                 {
                     s.push_back(line[j][same_i]);
                     same_i++;
@@ -71,10 +74,13 @@ int main() {
 
     }
 
-    
+    WordList WL =WordList();
 
+    int iterator_num = 1;
     for (list<string>::iterator it=line2.begin();it!=line2.end();it++)
     {
+       // SelectWord(*it,iterator_num);
+        iterator_num++;
         cout << *it << endl;
     }
 }
