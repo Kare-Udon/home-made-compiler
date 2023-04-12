ffc6165c236b51aaa6728e40df1d953cc66a1113
Kare-Udon <gpg@udon.eu.org>
2023-04-12 21:26:23 +0800

    Update: change format

--- Function.h
+++ Function.h
@@ -1,213 +1,186 @@
 #pragma once
 
 #include <string>
+
 using namespace std;
+
 #include "wordlist.h"
 
 bool IsNum(char s) {
-	if (isdigit(s) != 0)
-	{
-		return true;
-	}
+    if (isdigit(s) != 0)
+        return true;
+    else
+        return false;
 }
-bool IsSingleSymbol(char s) {
-	if ((isdigit(s) != 0) || (isalpha(s) != 0) )
 
-	{
-		return false;
-	}
-	else
-	{
-		return true;
-	}
+bool IsSingleSymbol(char s) {
+    if ((isdigit(s) != 0) || (isalpha(s) != 0)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 bool IsDualSymbol(char first, char next) {
-	if ((first == '+') && ((next == '=') || (next == '+')))
-	{
-		return true;
-	}
-	if ((first == '-') && ((next == '=') || (next == '-')))
-	{
-		return true;
-	}
-	if ((first == '*') && (next == '='))
-	{
-		return true;
-	}
-	if ((first == '/') && (next == '='))
-	{
-		return true;
-	}
-	if ((first == '%') && (next == '='))
-	{
-		return true;
-	}
-	if ((first == '!') && (next == '='))
-	{
-		return true;
-	}
-	if ((first == '>') && (next == '=') || (next == '>'))
-	{
-		return true;
-	}
-	if ((first == '<') && (next == '=') || (next == '<'))
-	{
-		return true;
-	}
-	if ((first == '&') && (next == '=') || (next == '&'))
-	{
-		return true;
-	}
-	if ((first == '|') && (next == '=') || (next == '|'))
-	{
-		return true;
-	}
-	if ((first == '^') && (next == '='))
-	{
-		return true;
-	}
-	if ((first == '=') && (next == '='))
-	{
-		return true;
-	}
-	return false;
+    if ((first == '+') && ((next == '=') || (next == '+'))) {
+        return true;
+    }
+    if ((first == '-') && ((next == '=') || (next == '-'))) {
+        return true;
+    }
+    if ((first == '*') && (next == '=')) {
+        return true;
+    }
+    if ((first == '/') && (next == '=')) {
+        return true;
+    }
+    if ((first == '%') && (next == '=')) {
+        return true;
+    }
+    if ((first == '!') && (next == '=')) {
+        return true;
+    }
+    if ((first == '>') && (next == '=') || (next == '>')) {
+        return true;
+    }
+    if ((first == '<') && (next == '=') || (next == '<')) {
+        return true;
+    }
+    if ((first == '&') && (next == '=') || (next == '&')) {
+        return true;
+    }
+    if ((first == '|') && (next == '=') || (next == '|')) {
+        return true;
+    }
+    if ((first == '^') && (next == '=')) {
+        return true;
+    }
+    if ((first == '=') && (next == '=')) {
+        return true;
+    }
+    return false;
 }
 
 
-bool IsNumber(const string& word) {
-	if (word.find('.') != string::npos) {
-		if (word.find_last_of('.') != word.find_first_of('.')) return false;
-		else if (word.find_first_of('.') == 0) return false;
-		else {
-			cout << "<" << word << ",float>" << endl;
-			return true;
-		}
-		
-	}
-	else {
-		int len = word.length();
-		int count = 0;
-		for (int j = 0; j < len; j++) {
-			if (isdigit(word[j])) { //判断字符是否是数字
-				count++;
-			}
-		}
-		if (count == len) {
-			cout << "<" << word << ",digit>" << endl;
-			return true;
-		}else  return false;
-		
-	}
-	
+bool IsNumber(const string &word) {
+    if (word.find('.') != string::npos) {
+        if (word.find_last_of('.') != word.find_first_of('.')) return false;
+        else if (word.find_first_of('.') == 0) return false;
+        else {
+            cout << "<" << word << ",float>" << endl;
+            return true;
+        }
+
+    } else {
+        int len = word.length();
+        int count = 0;
+        for (int j = 0; j < len; j++) {
+            if (isdigit(word[j])) { //判断字符是否是数字
+                count++;
+            }
+        }
+        if (count == len) {
+            cout << "<" << word << ",digit>" << endl;
+            return true;
+        } else return false;
+
+    }
+
 }
 
 
-bool Isdual_symbol(string word) {//先判断是不是二元符号，比如++ -- 
+bool IsDualSymbol(string word) {//先判断是不是二元符号，比如++ --
 
-	int len = word.length();
+    int len = word.length();
 
-	if (len == 2) {
+    if (len == 2) {
 
-		if (IsDualSymbol(word.at(0), word.at(1)))
-		{
-			cout << "<" << word << ",dual_symbol>" << endl;
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	else return false;
-}
-bool IsSingle_symbol(const string& word) {
-
-	if (Isdual_symbol(word))return true;
-	WordList wordList = *new WordList();
-
-	if (word.length() == 1) {
-
-		if (wordList.single_symbol.find(word) != wordList.single_symbol.end()) {
-			cout << "<" << word << ",Single_symbol>" << endl;
-			return true;
-			}
-		else
-			return false;
-	}
-	else
-		return false;
+        if (IsDualSymbol(word.at(0), word.at(1))) {
+            cout << "<" << word << ",dual_symbol>" << endl;
+            return true;
+        } else {
+            return false;
+        }
+    } else return false;
 }
 
-	if (len == 2) {
+bool IsSingleSymbol(const string &word) {
 
-bool Istype_identifier(string word) {
+    if (IsDualSymbol(word))return true;
+    WordList wordList = *new WordList();
 
-	//在类型标识符中查找
-	WordList wordList = *new WordList();
-	if (wordList.type_identifier.find(word) != wordList.type_identifier.end()) {
-		cout << "<" << word << ",ident>" << endl;
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+    if (word.length() == 1) {
+
+        if (wordList.single_symbol.find(word) != wordList.single_symbol.end()) {
+            cout << "<" << word << ",Single_symbol>" << endl;
+            return true;
+        } else
+            return false;
+    } else
+        return false;
 }
-bool Iskeywords(string word) {
-	WordList wordList = *new WordList();
 
-	//先判断是不是类型，再判断关键字
-	if (Istype_identifier(word)) {
+bool IsTypeIdentifier(const string& word) {
 
-		return true;
-	}
-	else if (wordList.keywords.find(word) != wordList.keywords.end()) {
-		cout << "<" << word << ",ident>" << endl;
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+    //在类型标识符中查找
+    WordList wordList = *new WordList();
+    if (wordList.type_identifier.find(word) != wordList.type_identifier.end()) {
+        cout << "<" << word << ",ident>" << endl;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool IsKeywords(const string& word) {
+    WordList wordList = *new WordList();
+
+    //先判断是不是类型，再判断关键字
+    if (IsTypeIdentifier(word)) {
+
+        return true;
+    } else if (wordList.keywords.find(word) != wordList.keywords.end()) {
+        cout << "<" << word << ",ident>" << endl;
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
 bool IsCorrectWord(string word) {
-	//不能数字开头，但main函数的读取已经限制了，被读取的word不会是数字开头
-	//不能是关键字等，Selctword前面已经排除了
-	bool all = true;
-	if (word.at(0) == '_ ' || islower(word.at(0))) {
-		for (int j = 1; j < word.length(); j++) {
-		if (word.at(j) == '_ '  ){ all = true; }//判断是不是下划线以及数字、字母    这里判断不是
-		else if(islower(word.at(j))){ all = true; }
-		else if(isdigit(word.at(j))) { all = true; }
-		else {	
-			all = false;
-			break;
-		}
-	}
-	}
-	else {	//判断是不是下划线以及数字、字母    这里判断不是
-		all = false;
-		
-	}
-	
-	if (all) {
-		cout << "<" << word << ",id>" << endl;
-	}
-	return all;
+    //不能数字开头，但main函数的读取已经限制了，被读取的word不会是数字开头
+    //不能是关键字等，SelectWord 前面已经排除了
+    bool all = true;
+    if (word.at(0) == '_' || islower(word.at(0))) {
+        for (int j = 1; j < word.length(); j++) {
+            if (word.at(j) == '_') { all = true; }  //判断是不是下划线以及数字、字母    这里判断不是
+            else if (islower(word.at(j))) { all = true; }
+            else if (isdigit(word.at(j))) { all = true; }
+            else {
+                all = false;
+                break;
+            }
+        }
+    } else {    //判断是不是下划线以及数字、字母    这里判断不是
+        all = false;
+
+    }
+
+    if (all) {
+        cout << "<" << word << ",id>" << endl;
+    }
+    return all;
 }
 
-void SelectWord(string word, int num) {
+void SelectWord(const string& word, int num) {
 
-	if (IsNumber(word)) return;  //判断这个string是不是数字
+    if (IsNumber(word)) return;         //判断这个string是不是数字
 
-//	if (Isdual_symbol(word)) return;//判断这个string是不是多符号
-	if (IsSingle_symbol(word)) return;//判断这个string是不是单符号
+    if (IsSingleSymbol(word)) return;   //判断这个string是不是单符号
 
-	if (Iskeywords(word)) return;///判断这个string是不是关键词
-	if (IsCorrectWord(word)) return;//判断这个string是不是正确的命名
+    if (IsKeywords(word)) return;       //判断这个string是不是关键词
+    if (IsCorrectWord(word)) return;    //判断这个string是不是正确的命名
 
-	cout << "<ERROR," << num <<"> "<< endl;
+    cout << "<ERROR," << num << "> " << endl;
 }
--- main.cpp
+++ main.cpp
@@ -3,41 +3,34 @@
 #include "Function.h"
 #include <list>
 #include "wordlist.h"
+
 using namespace std;
 
 
-
-
 int main() {
-    
-	ifstream fin("in.txt");
-    char buf[1024] = { 0 }; 
+
+    ifstream fin("in.txt");
+    char buf[1024] = {0};
     int num = 0;
     string line[1024];
-    while (fin >> buf)
-    {
+    while (fin >> buf) {
         line[num] = buf;
-      //  cout << line[num] << endl;//每一次的buf是空格或回车键（即白色字符）分开的元素
         num++;
     }
     //上述操作获得文件中，每次空格之间的元素
-    
-    list <string> line2;
-    for (int j = 0; j <= num; j++)
-    {
+
+    list<string> line2;
+    for (int j = 0; j <= num; j++) {
         int length = line[j].length();
         int i = 0;
-        while (i < length)
-        {
+        while (i < length) {
             if (line[j][i] == '/' && line[j][i + 1] == '/') {
                 i = length;
                 continue;
-            }
-            else if (IsNum(line[j][i]) || line[j][i] == '.') { //判断是不是数字 比如 12 1 1234
+            } else if (IsNum(line[j][i]) || line[j][i] == '.') { //判断是不是数字 比如 12 1 1234
                 string s;
                 int same_i = i;
-                while (IsNum(line[j][same_i])|| line[j][same_i]=='.'|| (isalpha(line[j][same_i]) != 0))
-                {
+                while (IsNum(line[j][same_i]) || line[j][same_i] == '.' || (isalpha(line[j][same_i]) != 0)) {
                     s.push_back(line[j][same_i]);
                     same_i++;
                     if (same_i >= length) //避免长度超过整行程度
@@ -45,30 +38,28 @@ int main() {
                 }
 
                 line2.push_back(s);
-                i=same_i;
+                i = same_i;
                 continue;
-            }
-            else if (IsSingleSymbol(line[j][i])) {//判断是不是符号
+            } else if (IsSingleSymbol(line[j][i])) {    //判断是不是符号
                 string s;
                 s.push_back(line[j][i]);
                 i++;
-                if (IsDualSymbol(line[j][i-1],line[j][i]))//判断是不是二元运算符如 ++ -- ,两个参数，一个是第一位参数，一个是第二位参数
+                if (IsDualSymbol(line[j][i - 1], line[j][i]))   //判断是不是二元运算符如 ++ -- ,两个参数，一个是第一位参数，一个是第二位参数
                 {
                     s.push_back(line[j][i]);
                     i++;
                 }
                 line2.push_back(s);
-                
+
                 continue;
-            }
-            else { //剩余就是字符串 一直读，读到换行
+            } else {    //剩余就是字符串 一直读，读到换行
                 string s;
                 int same_i = i;
-                while (same_i<length && !IsSingleSymbol(line[j][same_i]))//判断下一个是不是符号
+                while (same_i < length && !IsSingleSymbol(line[j][same_i])) //判断下一个是不是符号
                 {
                     s.push_back(line[j][same_i]);
                     same_i++;
-                  
+
                 }
                 line2.push_back(s);
                 i = same_i;
@@ -80,10 +71,8 @@ int main() {
 
 
     int iterator_num = 1;
-    for (list<string>::iterator it=line2.begin();it!=line2.end();it++)
-    {
-        SelectWord(*it,iterator_num);
+    for (auto & it : line2) {
+        SelectWord(it, iterator_num);
         iterator_num++;
-       // cout << *it << endl;
     }
 }
